09e2c423aac052a85b0cd0dd14a261d51d1db291
unknown <aanggaadc@gmail.com>
2022-07-10 19:01:17 +0700

    add scroll function for showing newsletter

--- main.js
+++ main.js
@@ -7,30 +7,49 @@ function closeNotification() {
     })
 }
 
+function showNewsLetter() {
+    const newsPanel = document.getElementById('newsletter')
+
+    function scrollShow() {
+        let scrollTop = window.scrollY;
+        let docHeight = document.body.offsetHeight;
+        let winHeight = window.innerHeight;
+        let scrollPercent = scrollTop / (docHeight - winHeight);
+        let scrollPercentRounded = Math.round(scrollPercent * 100);
+
+        if (scrollPercentRounded >= 33.33) {
+            newsPanel.classList.add('show')
+        } 
+    };
+
+    window.addEventListener("scroll", scrollShow);
+}
+
 function closeNewsLetter() {
     const newsPanel = document.getElementById('newsletter')
     const closeButton = document.getElementById('newsletter-close')
 
     closeButton.addEventListener('click', () => {
-        newsPanel.classList.add('close')
+        newsPanel.classList.remove('show')
+        newsPanel.style.visibility = "hidden"    
         window.sessionStorage.setItem('status', 'true')
         setTimeout(() => {
             window.sessionStorage.removeItem('status')
         }, 600000)
     })
-
 }
 
 function cekStatus() {
     const newsPanel = document.getElementById('newsletter')
-    const status = !window.sessionStorage.getItem('status')
-    if (status) {
+    const status = window.sessionStorage.getItem('status')
+    if (!status) {
         newsPanel.style.removeProperty('visibility')       
       } 
 }
 
 window.onload = function(){
     cekStatus()
+    showNewsLetter()
     closeNewsLetter()
     closeNotification()
 }
